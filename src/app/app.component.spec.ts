18aab283a4bae3b1e3aad076403be122752acd72
Kzu <alvaro.vega.pavez@gmail.com>
2024-11-17 23:15:24 -0300

    add: app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule // Importa RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ps-factory' title`, () => {
+  it(`should have as title 'ps-factory'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ps-factory');
@@ -24,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ps-factory');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Hello, ps-factory');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from './models/user.model';
+import { UserStateService } from './services/user-state.service';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ps-factory';
+  currentUser: User | null = null;
+
+  constructor(private router: Router, private userStateService: UserStateService) {}
+
+  ngOnInit(): void {
+    // Verificar si hay un usuario actual en localStorage y actualizar el estado
+    const savedUser = this.userStateService.getCurrentUser();
+    if (savedUser) {
+      this.currentUser = savedUser;
+    }
+
+    // Suscribirse al observable currentUser$ para actualizar currentUser cuando cambie el estado del usuario
+    this.userStateService.currentUser$.subscribe(user => {
+      this.currentUser = user;
+    });
+  }
+
+  logout(): void {
+    // Llamar a clearCurrentUser del UserStateService para cerrar la sesión
+    this.userStateService.clearCurrentUser();
+    this.router.navigate(['/login']);
+  }
 }
