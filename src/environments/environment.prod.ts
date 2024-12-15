cad31e399a57ee49b02d0afda9fd18aa5810f4c8
Kzu <alvaro.vega.pavez@gmail.com>
2024-12-15 07:50:02 -0300

    se cambia dominio

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrlUsuario: 'http://34.235.145.21:8095',
-  apiUrlProducto: 'http://34.235.145.21:8096',
-  apiUrlVenta: 'http://34.235.145.21:8097'
+  apiUrlUsuario: 'http://54.165.184.60:8095',
+  apiUrlProducto: 'http://54.165.184.60:8096',
+  apiUrlVenta: 'http://54.165.184.60:8097'
 };
